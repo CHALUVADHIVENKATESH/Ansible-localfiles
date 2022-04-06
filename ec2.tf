8eea75114bbcdf5a41eecb730cf02bc777d004f2
root <root@ip-172-31-10-167.us-east-2.compute.internal>
2022-04-06 19:03:06 +0000

    Ansible local_files

---
+++ ec2.tf
@@ -0,0 +1,71 @@
+resource "aws_instance" "web-1" {
+
+  ami = "ami-0fb653ca2d3203ac1"
+
+  availability_zone           = "us-east-2a"
+  instance_type               = "t2.micro"
+  key_name                    = "Ohio-Keypair3"
+  subnet_id                   = aws_subnet.subnet1-public.id
+  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  associate_public_ip_address = true
+  tags = {
+    Name       = "Server-1"
+    Env        = "Prod"
+    Owner      = "Sree"
+    CostCenter = "ABCD"
+  }
+  user_data = <<-EOF
+    #!/bin/bash
+    
+    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTGqh87brDDs2sjyhx94ggfJmS+MmD7yKZ2NdDcMoIb4xndXRsqL1TIJyGOA4cW3MLULDFummwGfdTj0PeGIara/0aMVcimJHzTRwgGvijmO6QwfefHnVtX45IKmEZJOLhYDXcBBxI2EFUoHiQuy51xMjemrQ/AENWoF+PYq/qmvedUibXZ63r+IhiUcAKQo8zOZEzYuK0eLB5Dq8VKijnhZfRh856fEQmNrsFbPWxhYFtPy4/2Fi4OEIs0h/pbQMLJHygwmil4W2cer9gZhdcOWuAIZQnm/8w2+5gYvc/LeXT5cKMThzjUCrYfyzMVVvR7pmMySjDeZH8q12xgpPffQw4Oq19J4LYXidyaB50cYJqIMb6pxj1FJuVZ02FWKfW6cnxpJ+feLFrf4lX+Iwr1xZNkYmaOHeOq3Spw/j26aaRPhG7IL9miTvq7zUlo8Q3Z7H+x+vRCICSUcT2nkszjmyqAcrbgAMHMHig57w0TOk6NN2zgY06NP+MM3pVsOU= root@ip-172-31-10-167' > /home/ubuntu/.ssh/authorized_keys    
+    EOF
+
+}
+
+resource "aws_instance" "web-2" {
+
+  ami = "ami-0fb653ca2d3203ac1"
+
+  availability_zone           = "us-east-2a"
+  instance_type               = "t2.micro"
+  key_name                    = "Ohio-Keypair3"
+  subnet_id                   = aws_subnet.subnet1-public.id
+  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  associate_public_ip_address = true
+  tags = {
+    Name       = "Server-2"
+    Env        = "Prod"
+    Owner      = "Sree"
+    CostCenter = "ABCD"
+  }
+  user_data = <<-EOF
+    #!/bin/bash
+    
+    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTGqh87brDDs2sjyhx94ggfJmS+MmD7yKZ2NdDcMoIb4xndXRsqL1TIJyGOA4cW3MLULDFummwGfdTj0PeGIara/0aMVcimJHzTRwgGvijmO6QwfefHnVtX45IKmEZJOLhYDXcBBxI2EFUoHiQuy51xMjemrQ/AENWoF+PYq/qmvedUibXZ63r+IhiUcAKQo8zOZEzYuK0eLB5Dq8VKijnhZfRh856fEQmNrsFbPWxhYFtPy4/2Fi4OEIs0h/pbQMLJHygwmil4W2cer9gZhdcOWuAIZQnm/8w2+5gYvc/LeXT5cKMThzjUCrYfyzMVVvR7pmMySjDeZH8q12xgpPffQw4Oq19J4LYXidyaB50cYJqIMb6pxj1FJuVZ02FWKfW6cnxpJ+feLFrf4lX+Iwr1xZNkYmaOHeOq3Spw/j26aaRPhG7IL9miTvq7zUlo8Q3Z7H+x+vRCICSUcT2nkszjmyqAcrbgAMHMHig57w0TOk6NN2zgY06NP+MM3pVsOU= root@ip-172-31-10-167' > /home/ubuntu/.ssh/authorized_keys    
+    EOF
+
+}
+
+resource "aws_instance" "web-3" {
+
+  ami = "ami-0fb653ca2d3203ac1"
+
+  availability_zone           = "us-east-2a"
+  instance_type               = "t2.micro"
+  key_name                    = "Ohio-Keypair3"
+  subnet_id                   = aws_subnet.subnet1-public.id
+  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  associate_public_ip_address = true
+  tags = {
+    Name       = "Server-3"
+    Env        = "Prod"
+    Owner      = "Sree"
+    CostCenter = "ABCD"
+  }
+  user_data = <<-EOF
+    #!/bin/bash
+    
+    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTGqh87brDDs2sjyhx94ggfJmS+MmD7yKZ2NdDcMoIb4xndXRsqL1TIJyGOA4cW3MLULDFummwGfdTj0PeGIara/0aMVcimJHzTRwgGvijmO6QwfefHnVtX45IKmEZJOLhYDXcBBxI2EFUoHiQuy51xMjemrQ/AENWoF+PYq/qmvedUibXZ63r+IhiUcAKQo8zOZEzYuK0eLB5Dq8VKijnhZfRh856fEQmNrsFbPWxhYFtPy4/2Fi4OEIs0h/pbQMLJHygwmil4W2cer9gZhdcOWuAIZQnm/8w2+5gYvc/LeXT5cKMThzjUCrYfyzMVVvR7pmMySjDeZH8q12xgpPffQw4Oq19J4LYXidyaB50cYJqIMb6pxj1FJuVZ02FWKfW6cnxpJ+feLFrf4lX+Iwr1xZNkYmaOHeOq3Spw/j26aaRPhG7IL9miTvq7zUlo8Q3Z7H+x+vRCICSUcT2nkszjmyqAcrbgAMHMHig57w0TOk6NN2zgY06NP+MM3pVsOU= root@ip-172-31-10-167' > /home/ubuntu/.ssh/authorized_keys    
+    EOF
+
+}
