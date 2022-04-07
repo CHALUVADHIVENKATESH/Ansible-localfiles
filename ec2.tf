4b36fc61fe87ec309014ff332a651f9eaca06cfe
root <root@ip-172-31-10-167.us-east-2.compute.internal>
2022-04-07 22:42:48 +0000

    added playbooks for copying file from 1 server to other server

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web-1" {
+resource "aws_instance" "master" {
 
   ami = "ami-0fb653ca2d3203ac1"
 
@@ -9,21 +9,21 @@ resource "aws_instance" "web-1" {
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   associate_public_ip_address = true
   tags = {
-    Name       = "Server-1"
+    Name       = "Docker-Master-01"
     Env        = "Prod"
     Owner      = "Sree"
     CostCenter = "ABCD"
   }
   user_data = <<-EOF
     #!/bin/bash
-    
-    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTGqh87brDDs2sjyhx94ggfJmS+MmD7yKZ2NdDcMoIb4xndXRsqL1TIJyGOA4cW3MLULDFummwGfdTj0PeGIara/0aMVcimJHzTRwgGvijmO6QwfefHnVtX45IKmEZJOLhYDXcBBxI2EFUoHiQuy51xMjemrQ/AENWoF+PYq/qmvedUibXZ63r+IhiUcAKQo8zOZEzYuK0eLB5Dq8VKijnhZfRh856fEQmNrsFbPWxhYFtPy4/2Fi4OEIs0h/pbQMLJHygwmil4W2cer9gZhdcOWuAIZQnm/8w2+5gYvc/LeXT5cKMThzjUCrYfyzMVVvR7pmMySjDeZH8q12xgpPffQw4Oq19J4LYXidyaB50cYJqIMb6pxj1FJuVZ02FWKfW6cnxpJ+feLFrf4lX+Iwr1xZNkYmaOHeOq3Spw/j26aaRPhG7IL9miTvq7zUlo8Q3Z7H+x+vRCICSUcT2nkszjmyqAcrbgAMHMHig57w0TOk6NN2zgY06NP+MM3pVsOU= root@ip-172-31-10-167' > /home/ubuntu/.ssh/authorized_keys    
+
+    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTGqh87brDDs2sjyhx94ggfJmS+MmD7yKZ2NdDcMoIb4xndXRsqL1TIJyGOA4cW3MLULDFummwGfdTj0PeGIara/0aMVcimJHzTRwgGvijmO6QwfefHnVtX45IKmEZJOLhYDXcBBxI2EFUoHiQuy51xMjemrQ/AENWoF+PYq/qmvedUibXZ63r+IhiUcAKQo8zOZEzYuK0eLB5Dq8VKijnhZfRh856fEQmNrsFbPWxhYFtPy4/2Fi4OEIs0h/pbQMLJHygwmil4W2cer9gZhdcOWuAIZQnm/8w2+5gYvc/LeXT5cKMThzjUCrYfyzMVVvR7pmMySjDeZH8q12xgpPffQw4Oq19J4LYXidyaB50cYJqIMb6pxj1FJuVZ02FWKfW6cnxpJ+feLFrf4lX+Iwr1xZNkYmaOHeOq3Spw/j26aaRPhG7IL9miTvq7zUlo8Q3Z7H+x+vRCICSUcT2nkszjmyqAcrbgAMHMHig57w0TOk6NN2zgY06NP+MM3pVsOU= root@ip-172-31-10-167' > /home/ubuntu/.ssh/authorized_keys
     EOF
 
 }
 
-resource "aws_instance" "web-2" {
-
+resource "aws_instance" "WorkerNode" {
+  count=3
   ami = "ami-0fb653ca2d3203ac1"
 
   availability_zone           = "us-east-2a"
@@ -33,21 +33,20 @@ resource "aws_instance" "web-2" {
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   associate_public_ip_address = true
   tags = {
-    Name       = "Server-2"
+    Name       = "Docker-WorkerNode-0${count.index+1}"
     Env        = "Prod"
     Owner      = "Sree"
     CostCenter = "ABCD"
   }
   user_data = <<-EOF
     #!/bin/bash
-    
-    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTGqh87brDDs2sjyhx94ggfJmS+MmD7yKZ2NdDcMoIb4xndXRsqL1TIJyGOA4cW3MLULDFummwGfdTj0PeGIara/0aMVcimJHzTRwgGvijmO6QwfefHnVtX45IKmEZJOLhYDXcBBxI2EFUoHiQuy51xMjemrQ/AENWoF+PYq/qmvedUibXZ63r+IhiUcAKQo8zOZEzYuK0eLB5Dq8VKijnhZfRh856fEQmNrsFbPWxhYFtPy4/2Fi4OEIs0h/pbQMLJHygwmil4W2cer9gZhdcOWuAIZQnm/8w2+5gYvc/LeXT5cKMThzjUCrYfyzMVVvR7pmMySjDeZH8q12xgpPffQw4Oq19J4LYXidyaB50cYJqIMb6pxj1FJuVZ02FWKfW6cnxpJ+feLFrf4lX+Iwr1xZNkYmaOHeOq3Spw/j26aaRPhG7IL9miTvq7zUlo8Q3Z7H+x+vRCICSUcT2nkszjmyqAcrbgAMHMHig57w0TOk6NN2zgY06NP+MM3pVsOU= root@ip-172-31-10-167' > /home/ubuntu/.ssh/authorized_keys    
+    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTGqh87brDDs2sjyhx94ggfJmS+MmD7yKZ2NdDcMoIb4xndXRsqL1TIJyGOA4cW3MLULDFummwGfdTj0PeGIara/0aMVcimJHzTRwgGvijmO6QwfefHnVtX45IKmEZJOLhYDXcBBxI2EFUoHiQuy51xMjemrQ/AENWoF+PYq/qmvedUibXZ63r+IhiUcAKQo8zOZEzYuK0eLB5Dq8VKijnhZfRh856fEQmNrsFbPWxhYFtPy4/2Fi4OEIs0h/pbQMLJHygwmil4W2cer9gZhdcOWuAIZQnm/8w2+5gYvc/LeXT5cKMThzjUCrYfyzMVVvR7pmMySjDeZH8q12xgpPffQw4Oq19J4LYXidyaB50cYJqIMb6pxj1FJuVZ02FWKfW6cnxpJ+feLFrf4lX+Iwr1xZNkYmaOHeOq3Spw/j26aaRPhG7IL9miTvq7zUlo8Q3Z7H+x+vRCICSUcT2nkszjmyqAcrbgAMHMHig57w0TOk6NN2zgY06NP+MM3pVsOU= root@ip-172-31-10-167' > /home/ubuntu/.ssh/authorized_keys
     EOF
 
 }
 
-resource "aws_instance" "web-3" {
-
+resource "aws_instance" "ExtraMasters" {
+  count=2
   ami = "ami-0fb653ca2d3203ac1"
 
   availability_zone           = "us-east-2a"
@@ -57,15 +56,15 @@ resource "aws_instance" "web-3" {
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   associate_public_ip_address = true
   tags = {
-    Name       = "Server-3"
+    Name       = "Docker-Master-0${count.index+2}"
     Env        = "Prod"
     Owner      = "Sree"
     CostCenter = "ABCD"
   }
   user_data = <<-EOF
     #!/bin/bash
-    
-    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTGqh87brDDs2sjyhx94ggfJmS+MmD7yKZ2NdDcMoIb4xndXRsqL1TIJyGOA4cW3MLULDFummwGfdTj0PeGIara/0aMVcimJHzTRwgGvijmO6QwfefHnVtX45IKmEZJOLhYDXcBBxI2EFUoHiQuy51xMjemrQ/AENWoF+PYq/qmvedUibXZ63r+IhiUcAKQo8zOZEzYuK0eLB5Dq8VKijnhZfRh856fEQmNrsFbPWxhYFtPy4/2Fi4OEIs0h/pbQMLJHygwmil4W2cer9gZhdcOWuAIZQnm/8w2+5gYvc/LeXT5cKMThzjUCrYfyzMVVvR7pmMySjDeZH8q12xgpPffQw4Oq19J4LYXidyaB50cYJqIMb6pxj1FJuVZ02FWKfW6cnxpJ+feLFrf4lX+Iwr1xZNkYmaOHeOq3Spw/j26aaRPhG7IL9miTvq7zUlo8Q3Z7H+x+vRCICSUcT2nkszjmyqAcrbgAMHMHig57w0TOk6NN2zgY06NP+MM3pVsOU= root@ip-172-31-10-167' > /home/ubuntu/.ssh/authorized_keys    
+
+    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTGqh87brDDs2sjyhx94ggfJmS+MmD7yKZ2NdDcMoIb4xndXRsqL1TIJyGOA4cW3MLULDFummwGfdTj0PeGIara/0aMVcimJHzTRwgGvijmO6QwfefHnVtX45IKmEZJOLhYDXcBBxI2EFUoHiQuy51xMjemrQ/AENWoF+PYq/qmvedUibXZ63r+IhiUcAKQo8zOZEzYuK0eLB5Dq8VKijnhZfRh856fEQmNrsFbPWxhYFtPy4/2Fi4OEIs0h/pbQMLJHygwmil4W2cer9gZhdcOWuAIZQnm/8w2+5gYvc/LeXT5cKMThzjUCrYfyzMVVvR7pmMySjDeZH8q12xgpPffQw4Oq19J4LYXidyaB50cYJqIMb6pxj1FJuVZ02FWKfW6cnxpJ+feLFrf4lX+Iwr1xZNkYmaOHeOq3Spw/j26aaRPhG7IL9miTvq7zUlo8Q3Z7H+x+vRCICSUcT2nkszjmyqAcrbgAMHMHig57w0TOk6NN2zgY06NP+MM3pVsOU= root@ip-172-31-10-167' > /home/ubuntu/.ssh/authorized_keys
     EOF
 
 }
--- local_file.tf
+++ local_file.tf
@@ -1,9 +1,13 @@
 resource "local_file" "hosts_cfg" {
   content = templatefile("hosts.tpl",
     {
-      server1 = aws_instance.web-1.public_ip
-      server2 = aws_instance.web-2.public_ip
-      server3 = aws_instance.web-3.public_ip
+      server1 = aws_instance.master.public_ip
+      server2 = aws_instance.ExtraMasters.0.public_ip
+      server3 = aws_instance.ExtraMasters.1.public_ip
+      server4 = aws_instance.WorkerNode.0.public_ip
+      server5 = aws_instance.WorkerNode.1.public_ip
+      server6 = aws_instance.WorkerNode.2.public_ip
+
     }
   )
   filename = "/root/terraformsingleinstance/hosts.cfg"
